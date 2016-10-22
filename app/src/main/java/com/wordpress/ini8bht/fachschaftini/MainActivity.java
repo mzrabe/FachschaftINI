039ae518c3efcdb3ff88c8dc635e5295a59649bf
MZahn <mzrabe@gmail.com>
2016-10-22 12:13:40 +0200

    room button implemented

--- app/src/main/java/com/wordpress/ini8bht/fachschaftini/MainActivity.java
+++ app/src/main/java/com/wordpress/ini8bht/fachschaftini/MainActivity.java
@@ -1,18 +1,30 @@
 package com.wordpress.ini8bht.fachschaftini;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     
-    TextView txt_header;
+    Button btn_roomaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_roomaction = (Button) findViewById(R.id.btn_room);
+
+        btn_roomaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RoomActivity.class));
+            }
+        });
 
     }
     
