c3ccd2b23625e41924015d64cff36afb9ed45350
MZahn <mzrabe@gmail.com>
2016-10-22 12:02:46 +0200

    Widget wird zurzeit einfach nur angezeigt. client implementierung muss noch gemacht werden

--- app/src/main/java/com/wordpress/ini8bht/fachschaftini/MainActivity.java
+++ app/src/main/java/com/wordpress/ini8bht/fachschaftini/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
     
     TextView txt_header;
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
     
     private void init(){
