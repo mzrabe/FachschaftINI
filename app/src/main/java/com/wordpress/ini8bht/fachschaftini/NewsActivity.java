7ac61409e8e6e654c09c4fe04aa29656ba59628a
TWille <wille_mail@posteo.de>
2016-10-22 14:36:59 +0200

    Website Link added

--- app/src/main/java/com/wordpress/ini8bht/fachschaftini/NewsActivity.java
+++ app/src/main/java/com/wordpress/ini8bht/fachschaftini/NewsActivity.java
@@ -1,12 +1,17 @@
 package com.wordpress.ini8bht.fachschaftini;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class NewsActivity extends AppCompatActivity {
 
     private WebView telegramWebView;
+    private Button btn_Website;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +19,19 @@ public class NewsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news);
 
         telegramWebView = (WebView)findViewById(R.id.telegram_WebView);
-      //  telegramWebView.createWebMessageChannel();
         telegramWebView.loadUrl("http://telegram.me/FSRVIII");
 
+        btn_Website = (Button) findViewById(R.id.button6);
+        btn_Website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.yourURL.com"));
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
