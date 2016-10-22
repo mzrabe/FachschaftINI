072a495f05dc5a23a52c5d026a17f5e2e7af6cd8
TWille <wille_mail@posteo.de>
2016-10-22 12:48:57 +0200

    news first try

--- app/src/main/java/com/wordpress/ini8bht/fachschaftini/MainActivity.java
+++ app/src/main/java/com/wordpress/ini8bht/fachschaftini/MainActivity.java
@@ -1,21 +1,37 @@
 package com.wordpress.ini8bht.fachschaftini;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     TextView txt_header;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View currentView) {
+                Intent i = new Intent(MainActivity.this, NewsActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
-    
+
     private void init(){
-        
+
     }
 }
--- app/src/main/java/com/wordpress/ini8bht/fachschaftini/NewsActivity.java
+++ app/src/main/java/com/wordpress/ini8bht/fachschaftini/NewsActivity.java
@@ -2,12 +2,21 @@ package com.wordpress.ini8bht.fachschaftini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private WebView telegramWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        telegramWebView = (WebView)findViewById(R.id.telegram_WebView);
+      //  telegramWebView.createWebMessageChannel();
+        telegramWebView.loadUrl("http://telegram.me/FSRVIII");
+
+
     }
 }
